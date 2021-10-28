be24c25a94c9efa8f07810d287ee7884aea5a4e6
Jon-Makkonahi <danila-bibik2017@yandex.ru>
2021-10-28 22:44:16 +0700

    first version

--- homework.py
+++ homework.py
@@ -1,15 +1,16 @@
-import logging
+import logging, os, requests, telegram, time
 
-...
+from dotenv import load_dotenv
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-CHAT_ID = ...
 
-...
+
+load_dotenv()
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 300
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+ENDPOINT = os.getenv('ENDPOINT')
 
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -17,38 +18,56 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена, в ней нашлись ошибки.'
 }
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='program.log', 
+    format='%(asctime)d, %(levelname)s, %(message)s'
+)
 
 def send_message(bot, message):
-    ...
+    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
 
 
 def get_api_answer(url, current_timestamp):
-    ...
-
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    date = {'from_date': current_timestamp or time.time()}
+    response = requests.get(url, headers=headers, params=date)
+    if response.status_code == 200:
+        return response.json()
+    logging.error('Код не 200!')
+    raise Exception
+        
 
 def parse_status(homework):
-    verdict = ...
-    ...
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    homework_name = homework['homework_name']
+    verdict = HOMEWORK_STATUSES[homework['status']]
+    message = f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    return message
 
 
 def check_response(response):
-    homeworks = response.get('homeworks')
-    ...
-
+    homework = response.get('homeworks')[0]
+    if homework['status'] not in HOMEWORK_STATUSES.keys():
+        logging.error('Неправильный ключ')
+        raise Exception
+    if homework is None:
+        logging.error('Домашка не изменилась')
+        raise Exception
+    return homework
 
 def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    ...
     while True:
         try:
-            ...
+            response = get_api_answer(ENDPOINT, current_timestamp)
+            homework = check_response(response)
+            message = parse_status(homework)
+            send_message(bot, message)
             time.sleep(RETRY_TIME)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            send_message(bot, message)
             time.sleep(RETRY_TIME)
             continue
 
