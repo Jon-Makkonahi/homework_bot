737f3f44ad6340134a5def89496bf0eca55d7ed4
Jon-Makkonahi <danila-bibik2017@yandex.ru>
2021-11-01 17:39:14 +0700

    fix no good

--- homework.py
+++ homework.py
@@ -3,7 +3,6 @@ import os
 import time
 
 from dotenv import load_dotenv
-from logging.handlers import RotatingFileHandler
 import requests
 import telegram
 
@@ -14,7 +13,11 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
-ENDPOINT = os.getenv('ENDPOINT')
+
+MESSAGE = 'Изменился статус проверки работы "{homework_name}". {verdict}'
+ERR = 'Не удалось получить ответ: {err}'
+SEND_MESSAGE = 'Не удалось отправить сообщение: {e}'
+ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -26,49 +29,54 @@ HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 def send_message(bot, message):
     """Отправка сообщения в телеграмм."""
-    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    except Exception as e:
+        print(SEND_MESSAGE.format(e=e))
+        logging.error('Произошло исключение', exc_info=True)
 
 
 def get_api_answer(url, current_timestamp):
     """Отправлять запрос к API домашки на эндпоинт."""
-    date = {'from_date': current_timestamp}
-    response = requests.get(url, headers=HEADERS, params=date)
-    if response.status_code == 200:
-        return response.json()
-    raise ConnectionError('Код не 200!')
+    try:
+        date = {'from_date': current_timestamp}
+        response = requests.get(url, headers=HEADERS, params=date)
+        if response.status_code == 200:
+            return response.json()
+        raise ValueError('Код не 200!')
+    except Exception as err:
+        print(ERR.format(err=err))
+        logging.error('Произошло исключение', exc_info=True)
+    if 'code' in response:
+        logging.error('Ошибка с сертификацией', exc_info=True)
+        raise KeyError('Ошибка с сертификацией')
+    if 'error' in response:
+        logging.error('Ошибка по времени', exc_info=True)
+        raise KeyError('Ошибка по времени')
 
 
 def parse_status(homework):
     """Проверка изменения статуса."""
-    homework_name = homework['homework_name']
-    verdict = HOMEWORK_VERDICTS[homework['status']]
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    return MESSAGE.format(
+        homework_name = homework['homework_name'],
+        verdict = HOMEWORK_VERDICTS[homework['status']]
+    )
 
 
 def check_response(response):
     """Проверять полученный ответ на корректность."""
     homework = response.get('homeworks')[0]
-    if homework['status'] not in HOMEWORK_VERDICTS.keys():
-        raise KeyError('Неправильный ключ')
-    if homework is None:
-        raise ValueError('Домашка не изменилась')
+    if homework['status'] not in HOMEWORK_VERDICTS:
+        logging.error('Неверный статус д/з', exc_info=True)
+        raise KeyError('Неверный статус д/з')
     return homework
 
 
 def main():
     """Выполение."""
-    logging.basicConfig(
-        level=logging.ERROR,
-        filename='program.log',
-        format='%(asctime)s, %(levelname)s, %(message)s, %(lineno)s'
-    )
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.ERROR)
-    handler = RotatingFileHandler('program.log', maxBytes=50000, backupCount=5)
-    logger.addHandler(handler)
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time()) - RETRY_TIME
     while True:
+        current_timestamp = int(time.time()) - RETRY_TIME
         try:
             response = get_api_answer(ENDPOINT, current_timestamp)
             homework = check_response(response)
@@ -78,7 +86,13 @@ def main():
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             send_message(bot, message)
-
+            time.sleep(RETRY_TIME)
 
 if __name__ == '__main__':
     main()
+    logging.basicConfig(
+        level=logging.ERROR,
+        filename='homework_bot/program.log',
+        format='%(asctime)s, %(levelname)s, %(lineno)s, %(message)s',
+        handlers=[logging.StreamHandler(), 'handler.log']
+    )
